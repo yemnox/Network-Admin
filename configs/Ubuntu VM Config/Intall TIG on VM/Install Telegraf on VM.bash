e68b093bd2db6f0e48b1cd563b16e144a9793ade
yemnox <emna.drihem@insat.ucar.tn>
2025-05-04 17:43:14 +0100

    Finish Telegraf install on VM

--- configs/Ubuntu VM Config/Intall TIG on VM/Install Telegraf on VM.bash
+++ configs/Ubuntu VM Config/Intall TIG on VM/Install Telegraf on VM.bash
@@ -23,4 +23,34 @@ systemctl enable --now telegraf
 systemctl status telegraf
 
 #Edit Config file
-sudo nano/etc/telegraf/telegraf.conf
+sudo nano /etc/telegraf/telegraf.conf
+
+#Create SNMP config file
+sudo nano /etc/telegraf/telegraf.d/monitor-linux-snmp.conf
+    #Paste this code:
+[[inputs.snmp]]
+  agents = [ "127.0.0.1:161" ] # IP:Port of the SNMP agent
+  version = 2
+  community = "public"         # Replace with your SNMP community name
+  name = "snmp"
+
+  [[inputs.snmp.field]]
+    name = "hostname"
+    oid = "RFC1213-MIB::sysName.0"
+    is_tag = true
+
+  [[inputs.snmp.table]]
+    name = "snmp"
+    inherit_tags = [ "hostname" ]
+    oid = "IF-MIB::ifXTable"
+
+    [[inputs.snmp.table.field]]
+      name = "ifName"
+      oid = "IF-MIB::ifName"
+      is_tag = true
+
+
+#restart telegraf
+sudo systemctl restart telegraf
+#test telegraf config
+sudo telegraf -config /etc/telegraf/telegraf.d/monitor-linux-snmp.conf --test
